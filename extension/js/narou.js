5de8ae81aa02d8c88e1887338bf6a07283c95273
fujita <fujita@nulab-inc.com>
2018-05-06 01:01:56 +0900

    リーダーの挙動を共通化

--- extension/js/narou.js
+++ extension/js/narou.js
@@ -1,59 +1,5 @@
 const reader = $('#novel_honbun');
 if (reader.length) {
-    const VK_SPACE = 32;
-    const VK_HOME = 36;
-    const VK_END = 35;
-    const VK_RIGHT = 39;
-    const VK_LEFT = 37;
-    const VK_PAGEDOWN = 34;
-    const VK_PAGEUP = 33;
-    const VK_N = 78;
-    const VK_P = 80;
-    const VK_B = 66;
-
-    var locked = false;
-    const scrollLeftBy = (h) => {
-        if (!locked) {
-            locked = true;
-            const sl = reader.scrollLeft() - h;
-            reader.animate({scrollLeft: sl}, () => {locked = false});
-        }
-    }
-    const pageUp = (rate = 1.0) => {
-        const pw = reader.width() - 50;
-        scrollLeftBy(-pw * rate);
-    }
-    const pageDown = (rate = 1.0) => {
-        const pw = reader.width() - 50;
-        scrollLeftBy(pw * rate);
-    }
-    const stop = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-    }
-    const jump = (i, a) => {
-        location.href = a.href;
-    }
-    const mapping = {};
-    mapping[VK_SPACE] = (e) => e.shiftKey ? pageUp() : pageDown();
-    mapping[VK_PAGEUP] = pageUp;
-    mapping[VK_PAGEDOWN] = pageDown;
-    mapping[VK_RIGHT] = (e) => e.shiftKey ? pageUp(0.5) : scrollLeftBy(-200);
-    mapping[VK_LEFT] = (e) => e.shiftKey ? pageDown(0.5) : scrollLeftBy(200);
-    mapping[VK_HOME] = (e) => scrollLeftBy(99999);
-    mapping[VK_END] = (e) => scrollLeftBy(-99999);
-    mapping[VK_P] = (e) => $('.js_prev-link:eq(0)').each(jump);
-    mapping[VK_N] = (e) => $('.js_next-link:eq(0)').each(jump);
-    mapping[VK_B] = (e) => $('.set_siori:eq(0)').click();
-
-    $(document).scrollTop(reader.offset().top - 100)
-    .keydown((e) => {
-        const func = mapping[e.which];
-        if (func) {
-            func(e);
-            stop(e);
-        }
-    });
     $('.novel_bn a').each((i, elem) => {
         const e = $(elem);
         const t = e.text();
@@ -62,5 +8,11 @@ if (reader.length) {
         } else if (t.match(/<</)) {
             e.text(t.replace(/<</, '') + ' >>').addClass('js_prev-link');
         }
+    });
+    new NovelReader({
+        reader: reader,
+        prevFunc: () => $('.js_prev-link').jump(),
+        nextFunc: () => $('.js_next-link').jump(),
+        bookmarkFunc: () => $('.set_siori:eq(0)').click()
     });
 }
---
+++ extension/js/novel-reader.js
@@ -0,0 +1,63 @@
+
+$.fn.jump = function() {
+    if (this[0] && this[0].href) {
+        location.href = this[0].href;
+    }
+}
+
+class NovelReader {
+    constructor(conf) {
+        this.reader = conf.reader;
+        this.prevFunc = conf.prevFunc;
+        this.nextFunc = conf.nextFunc;
+        this.bookmarkFunc = conf.bookmarkFunc;
+        $(document).scrollTop(this.reader.offset().top - 100);
+        this._initKeyMap();
+    }
+    scrollLeftBy(h) {
+        if (!this.locked) {
+            this.locked = true;
+            const sl = this.reader.scrollLeft() - h;
+            this.reader.animate({scrollLeft: sl}, () => {this.locked = false});
+        }
+    }
+    pageUp(rate = 1.0) {
+        const pw = this.reader.width() - 50;
+        this.scrollLeftBy(-pw * rate);
+    }
+    pageDown(rate = 1.0) {
+        const pw = this.reader.width() - 50;
+        this.scrollLeftBy(pw * rate);
+    }
+    _initKeyMap() {
+        const VK_SPACE = 32;
+        const VK_HOME = 36;
+        const VK_END = 35;
+        const VK_RIGHT = 39;
+        const VK_LEFT = 37;
+        const VK_PAGEDOWN = 34;
+        const VK_PAGEUP = 33;
+        const VK_N = 78;
+        const VK_P = 80;
+        const VK_B = 66;
+        const mapping = {}
+        mapping[VK_SPACE] = (e) => e.shiftKey ? this.pageUp() : this.pageDown();
+        mapping[VK_PAGEUP] = this.pageUp.bind(this);
+        mapping[VK_PAGEDOWN] = this.pageDown.bind(this);
+        mapping[VK_RIGHT] = (e) => e.shiftKey ? this.pageUp(0.5) : this.scrollLeftBy(-200);
+        mapping[VK_LEFT] = (e) => e.shiftKey ? this.pageDown(0.5) : this.scrollLeftBy(200);
+        mapping[VK_HOME] = (e) => this.scrollLeftBy(99999);
+        mapping[VK_END] = (e) => this.scrollLeftBy(-99999);
+        mapping[VK_P] = this.prevFunc;
+        mapping[VK_N] = this.nextFunc;
+        mapping[VK_B] = this.bookmarkFunc;
+        $(document).keydown((e) => {
+            const func = mapping[e.which];
+            if (func) {
+                func(e);
+                e.preventDefault();
+                e.stopPropagation();
+            }
+        });
+    }
+}
