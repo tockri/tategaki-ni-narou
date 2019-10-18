9c797f86aaa7e92af594a7fcc7bfb960c2e9eb85
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2019-10-19 00:30:12 +0900

    Chromeストア、Firefoxに公開

--- extension/js/kakuyomu.js
+++ extension/js/kakuyomu.js
@@ -1,12 +1,15 @@
-const reader = $('.js-episode-body');
-if (reader.length) {
-    $(document).scrollTop(0);
-    reader.prepend($('#contentMain-header').remove());
-    NovelReader(reader, {
-        prevFunc: () => $('#contentMain-previousEpisode>a').jump(),
-        nextFunc: () => $('#contentMain-nextEpisode>a').jump(),
-        bookmarkFunc: null,
-        myPageFunc: () => location.href = 'https://kakuyomu.jp/my/antenna/works',
-        indexFunc: () => $('#worksEpisodesEpisodeHeader-breadcrumbs a:first-child').jump()
-    });
-}
+$(() => {
+    const reader = $('.js-episode-body');
+    if (reader.length) {
+        $(document).scrollTop(0);
+        reader.prepend($('#contentMain-header').remove());
+        NovelReader(reader, {
+            prevFunc: () => $('#contentMain-previousEpisode>a').jump(),
+            nextFunc: () => $('#contentMain-nextEpisode>a').jump(),
+            bookmarkFunc: null,
+            myPageFunc: () => location.href = 'https://kakuyomu.jp/my/antenna/works',
+            indexFunc: () => $('#worksEpisodesEpisodeHeader-breadcrumbs a:first-child').jump()
+        });
+    }
+});
+
--- extension/js/narou.js
+++ extension/js/narou.js
@@ -1,21 +1,23 @@
-const reader = $('#novel_honbun');
-if (reader.length) {
-    $('.novel_bn a').each((i, elem) => {
-        const e = $(elem);
-        const t = e.text();
-        if (t.match(/>>/)) {
-            e.text('<< ' + t.replace(/>>/, '')).addClass('js_next-link').show();
-        } else if (t.match(/<</)) {
-            e.text(t.replace(/<</, '') + ' >>').addClass('js_prev-link').show();
-        }
-    });
-    reader.prepend($('#novel_p').remove())
-    .prepend($('p.novel_subtitle').remove());
-    NovelReader(reader, {
-        prevFunc: () => $('.js_prev-link').jump(),
-        nextFunc: () => $('.js_next-link').jump(),
-        bookmarkFunc: () => $('.set_siori:eq(0)').click(),
-        myPageFunc: () => $('.userfavnovellist').jump(),
-        indexFunc: () => $('#container>.contents1>a:eq(0)').jump()
-    });
-}
+$(() => {
+    const reader = $('#novel_honbun');
+    if (reader.length) {
+        $('.novel_bn a').each((i, elem) => {
+            const e = $(elem);
+            const t = e.text();
+            if (t.match(/>>/)) {
+                e.text('<< ' + t.replace(/>>/, '')).addClass('js_next-link').show();
+            } else if (t.match(/<</)) {
+                e.text(t.replace(/<</, '') + ' >>').addClass('js_prev-link').show();
+            }
+        });
+        reader.prepend($('#novel_p').remove())
+        .prepend($('p.novel_subtitle').remove());
+        NovelReader(reader, {
+            prevFunc: () => $('.js_prev-link').jump(),
+            nextFunc: () => $('.js_next-link').jump(),
+            bookmarkFunc: () => $('.set_siori:eq(0)').click(),
+            myPageFunc: () => $('.userfavnovellist').jump(),
+            indexFunc: () => $('#container>.contents1>a:eq(0)').jump()
+        });
+    }
+});
