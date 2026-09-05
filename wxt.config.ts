d896f70ea2722b01953a8df31899821b2b464924
tockri <trickster.mf@gmail.com>
2026-09-05 18:11:54 +0900

    2.1.13

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   webExt: { startUrls: ["https://ncode.syosetu.com/n6970df/1/"] },
   manifest: {
     name: "縦書きになろう",
-    version: "2.1.12",
     description: "「小説家になろう」と「カクヨム」を縦書きで読みやすくします。",
     web_accessible_resources: [{ resources: ["icon/48.png"], matches: ["<all_urls>"] }]
   }
